e74e7269fd29f6957f2362cc108bcece24b36388
tomasba <tomas.baltrukonis@gmail.com>
2026-09-10 20:36:42 +0300

    multiple consumers per application

--- src/main/java/com/learnkafka/config/LibraryEventsConsumerConfig.java
+++ src/main/java/com/learnkafka/config/LibraryEventsConsumerConfig.java
@@ -1,10 +1,37 @@
 package com.learnkafka.config;
 
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.kafka.autoconfigure.ConcurrentKafkaListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.ContainerCustomizer;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.listener.ContainerProperties;
 
 @Configuration
-//@EnableKafka
+@EnableKafka
 public class LibraryEventsConsumerConfig {
+
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<Object, Object> kafkaListenerContainerFactory(
+            ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
+            ConsumerFactory<Object, Object> kafkaConsumerFactory) {
+        ConcurrentKafkaListenerContainerFactory<Object, Object> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        // Apply Boot's standard configuration first
+        configurer.configure(factory, kafkaConsumerFactory);
+        // configure multiple consumer instances in application. set to 3 as we have 3 partitions per topic
+        // might be applied like this if not running in a cloud like environment (i.e. no kubernetes)
+        factory.setConcurrency(3);
+//        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL_IMMEDIATE);
+        // add error handler, etc.
+
+        return factory;
+    }
 
 }
