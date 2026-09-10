bc606c542b60695ac506d7d3e1e4a716ea8f999f
tomasba <tomas.baltrukonis@gmail.com>
2026-09-10 15:22:23 +0300

    consume library event. consumer impl

---
+++ src/main/java/com/learnkafka/consumer/LibraryEventsConsumer.java
@@ -0,0 +1,21 @@
+package com.learnkafka.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LibraryEventsConsumer {
+
+    Logger log = LoggerFactory.getLogger(LibraryEventsConsumer.class);
+
+    @KafkaListener(topics = {"${spring.kafka.topic:library-events}"}
+            , autoStartup = "${libraryListener.startup:true}"
+            , groupId = "library-events-listener-group")
+    public void onMessage(ConsumerRecord<Integer, String> record) {
+        log.info("Received Library Events record: {}", record.value());
+    }
+
+}
