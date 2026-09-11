6523bc59b9d47f8eee220ef3a7e34025638750b3
tomasba <tomas.baltrukonis@gmail.com>
2026-09-11 08:33:56 +0300

    init consumer iTest setup

---
+++ src/test/java/com/learnkafka/consumer/LibraryEventsConsumerIT.java
@@ -0,0 +1,39 @@
+package com.learnkafka.consumer;
+
+import org.junit.jupiter.api.AutoClose;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.MessageListenerContainer;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.ContainerTestUtils;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@EmbeddedKafka(partitions = 3, topics = {"library-events"})
+@ActiveProfiles("itest")
+public class LibraryEventsConsumerIT {
+
+    @Autowired
+    private EmbeddedKafkaBroker embeddedKafkaBroker;
+
+    @Autowired
+    private KafkaTemplate<Integer, String> kafkaTemplate;
+
+    @Autowired
+    private KafkaListenerEndpointRegistry kafkaListenerEndpointRegistry;
+
+    @BeforeEach
+    public void setup() {
+        for (MessageListenerContainer messageListenerContainer : kafkaListenerEndpointRegistry.getListenerContainers()) {
+            ContainerTestUtils.waitForAssignment(messageListenerContainer, embeddedKafkaBroker.getPartitionsPerTopic());
+        }
+    }
+
+    @Test
+    void contextLoads() {}
+}
